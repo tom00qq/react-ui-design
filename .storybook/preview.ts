2bf274b2caf34640e7a5c19534fcdf6f709cfd30
TomHung <tomworkpurpose@gmail.com>
2025-08-10 23:39:12 +0800

    update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,17 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
+
+import eruda from "eruda";
+
+eruda.init();
+eruda.show("console");
+eruda.position({ x: window.innerWidth - 60, y: 20 });
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -13,8 +19,8 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
 };
 
--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,10 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./datePicker.css";
 
 const DatePicker = () => {
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
+  const currentDate = today.getDate();
+
+  const getFirstDayOfMonth = (year: number, month: number) => {
+    const monthIndex = month - 1;
+    return new Date(year, monthIndex, 1).getDay();
+  };
+
+  const getDayList = (year: number, month: number) => {
+    const monthIdex = month - 1;
+    const lastDate = new Date(year, monthIdex, 0).getDate();
+    return Array.from({ length: lastDate }, (_, i) => i + 1);
+  };
+
   const [date, setDate] = useState("");
   const [show, setShow] = useState(false);
+
+  const [yearList] = useState<number[]>(() => {
+    const years = [];
+    const startYear = currentYear - 100;
+    const endYear = currentYear + 10;
+
+    for (let i = startYear; i <= endYear; i++) {
+      years.push(i);
+    }
+
+    return years;
+  });
+
+  const monthList = Array.from({ length: 12 }, (_, i) => i + 1);
+  const [dayList, setDayList] = useState<number[]>(() =>
+    getDayList(currentYear, currentMonth)
+  );
+
+  const [fillDayArray, setFillDayArray] = useState(
+    Array.from(
+      { length: getFirstDayOfMonth(currentYear, currentMonth) },
+      (_, i) => i + 1
+    )
+  );
+
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+  const [selectedDate, setSelectedDate] = useState(currentDate);
+
+  useEffect(() => {
+    setDayList(getDayList(selectedYear, selectedMonth));
+    setFillDayArray(
+      Array.from(
+        { length: getFirstDayOfMonth(selectedYear, selectedMonth) },
+        (_, i) => i + 1
+      )
+    );
+  }, [selectedYear, selectedMonth]);
 
   return (
     <div className="date-picker">
@@ -22,7 +76,22 @@ const DatePicker = () => {
             <button type="button" className="prev-btn">
               ←
             </button>
-            2025
+            <select
+              className="yearList"
+              name="year"
+              id="year"
+              value={selectedYear}
+              onChange={(e) => {
+                setSelectedYear(Number(e.target.value));
+              }}
+            >
+              {yearList.map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+
             <button type="button" className="next-btn">
               →
             </button>
@@ -31,7 +100,21 @@ const DatePicker = () => {
             <button type="button" className="prev-btn">
               ←
             </button>
-            8
+            <select
+              className="monthList"
+              name="month"
+              id="month"
+              value={selectedMonth}
+              onChange={(e) => {
+                setSelectedMonth(Number(e.target.value));
+              }}
+            >
+              {monthList.map((month) => (
+                <option key={month} value={month}>
+                  {month}
+                </option>
+              ))}
+            </select>
             <button type="button" className="next-btn">
               →
             </button>
@@ -44,9 +127,19 @@ const DatePicker = () => {
             <span>四</span>
             <span>五</span>
             <span>六</span>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
+            {fillDayArray.map((fillDay) => {
+              return <span key={fillDay}></span>;
+            })}
+            {dayList.map((day) => {
+              return (
+                <span
+                  key={day}
+                  className={day === selectedDate ? "selected" : ""}
+                >
+                  {day}
+                </span>
+              );
+            })}
           </div>
         </div>
       ) : (
@@ -57,3 +150,14 @@ const DatePicker = () => {
 };
 
 export default DatePicker;
+
+// 學習重點
+// 1. 年份資料來源，可以用 today 當年年份為基準點，往前、後推
+// 2. 元件生命週期
+// 3. Array.from
+// 4. getDaysOfMonth
+
+// 疑惑觀念
+// 1. useEffect 同樣依賴應該寫一起嗎？
+//    useEffect 即便相同依賴，但關注點不同就應該分離，寫複數個
+// 2. setState 批次更新、updater func、re-render
